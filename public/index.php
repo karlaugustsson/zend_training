e7178603d2b0e0ec0ef8538cbe0169f29d3c09e6
Karl Augustsson <karl.augustsson@gmail.com>
2016-08-16 17:50:02 +0200

    one more to go

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 /**
  * Display all errors when APPLICATION_ENV is development.
  */
+
 if ($_SERVER['APPLICATION_ENV'] == 'development') {
     error_reporting(E_ALL);
     ini_set("display_errors", 1);
